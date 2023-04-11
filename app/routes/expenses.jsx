d9604f6259c356bdd89d75b98ca7564a85bd24be
abid982 <abidarif982@gmail.com>
2023-04-11 12:29:32 +0500

    added components and styling

--- app/routes/expenses.jsx
+++ app/routes/expenses.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from '@remix-run/react';
 
+import expensesStyles from '~/styles/expenses.css';
+
 export default function ExpensesLayout() {
     return <main>
         <p>Shared element!</p>
         <Outlet/>
     </main>
-  }
+}
+
+export function links() {
+    return [{ rel: 'stylesheet', href: expensesStyles }];
+}
