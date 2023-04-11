c48c066ab8734c4a95b8e34d63ae6fb78d87ce60
abid982 <abidarif982@gmail.com>
2023-04-11 14:32:06 +0500

    add header to main layout and wrap modal to expense form component

--- app/root.jsx
+++ app/root.jsx
@@ -7,6 +7,8 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 
+import MainHeader from '~/components/navigation/MainHeader';
+
 import sharedStyles from '~/styles/shared.css';
 
 export const meta = () => ({
@@ -34,6 +36,7 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <MainHeader />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
